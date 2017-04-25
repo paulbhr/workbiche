6fdb02abbfc035c72c4ffa4ad859067ee7ae0528
paulbhr <paulbh.roche@gmail.com>
2017-04-25 12:13:28 +0200

    maquette

---
+++ database.sql
@@ -0,0 +1,6 @@
+create database if not exists workbiche character set utf8 collate utf8_unicode_ci;
+
+use workbiche;
+
+
+grant all privileges on workbiche.* to 'workbiche'@'localhost' identified by 'Coucou1$';
